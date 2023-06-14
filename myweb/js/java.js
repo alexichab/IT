fd4f21a16f927d4959c760a48891149d165aaf34
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-06-15 02:31:52 +0700

    Add my first web

---
+++ myweb/js/java.js
@@ -0,0 +1,15 @@
+function menu() {
+ var menu = document.getElementById("menu");
+ var content = document.getElementById("content");
+ menu.classList.toggle("menu_active");
+ content.classList.toggle("content_active");
+}
+menu.oneclick = function myFunction(){
+
+	var x = document.getElementById("my")
+}
+
+window.addEventListener('load', function() {
+    var bear = document.querySelector(".bear");
+    bear.classList.add("bear_vis");
+});
